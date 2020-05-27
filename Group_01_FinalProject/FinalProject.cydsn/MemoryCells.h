4dda060d8e576a307f1011d786090c2c3a9ade8e
Teo <teobistoni@hotmail.it>
2020-05-27 11:22:58 +0200

    Memory_cells.h created to define and specify the EEPROM cells' addresses useful for the initial configuration

---
+++ Group_01_FinalProject/FinalProject.cydsn/MemoryCells.h
@@ -0,0 +1,23 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#ifndef MEMORY_CELLS_H
+    #define MEMORY_CELLS_H
+    
+    #define FLAG_ADDRESS 0x0000
+    #define POINTER_ADDRESS 0x0001
+    #define BEGIN_STOP_ADDRESS 0x0002
+    #define FULL_SCALE_RANGE_ADDRESS 0x0003
+    #define SAMPLING_FREQUENCY_ADDRESS 0x0004
+    #define TEMPERATURE_UNIT_ADDRESS 0x0005
+    
+#endif
+/* [] END OF FILE */
