984d5309d1ef468cda4537f2a0bbd4e714d17623
Teo <teobistoni@hotmail.it>
2020-05-28 18:43:52 +0200

    Issue on main solved

--- Group_01_FinalProject/FinalProject.cydsn/main.c
+++ Group_01_FinalProject/FinalProject.cydsn/main.c
@@ -1,3 +1,4 @@
+
 /* ========================================
  *
  * Copyright YOUR COMPANY, THE YEAR
@@ -16,6 +17,7 @@
 #include "I2C_Interface.h"
 #include "25LC256.h"
 #include "MemoryCells.h"
+#include "EEPROMCommunication.h"
 
 /* Data from the temperature sensors have to be converted in Celsius as:
 * /             (value_mv - 500)* 0.1 
@@ -38,6 +40,7 @@ float q_temp_conversion;
 
 int main(void)
 {
+    CyGlobalIntEnable; /* Enable global interrupts. */
     
     /****INITIAL EEPROM CONFIGURATION****/
 
@@ -53,81 +56,36 @@ int main(void)
     
     UART_PutString("\nUART Started\r\n");
     
+     isr_FIFO_StartEx(Custom_isr_FIFO);
     isr_TIMER_StartEx(Custom_isr_TIMER);
+    
     ADC_DelSig_StartConvert();
         
     
     uint8 Flag_cell;
-    uint16 pointer;
     
     Flag_cell = EEPROM_readByte(FLAG_ADDRESS);
     
-    if(Flag_cell == 0){
-        UART_PutString("Setting EEPROM registers at default value\r\n");    
-        /*Setting EEPROM registers at default value*/
-        pointer = FIRST_FREE_CELL;
-        EEPROM_writeByte(POINTER_ADDRESS_H,(pointer&0xFF00)>>8);
-        UART_PutString("Pointer first register set\r\n");
-        EEPROM_writeByte(POINTER_ADDRESS_L,(pointer&0xff));
-        UART_PutString("Pointer second registers set\r\n");
-        EEPROM_writeByte(BEGIN_STOP_ADDRESS,0);
-        UART_PutString("Begin/stop cell set in 'stop' condition\r\n");
-        EEPROM_writeByte(FULL_SCALE_RANGE_ADDRESS,1);
-        UART_PutString("Full scale range set at +/-2g\r\n");
-        EEPROM_writeByte(SAMPLING_FREQUENCY_ADDRESS,1);
-        UART_PutString("Sampling frequency set at 1Hz\r\n");
-        EEPROM_writeByte(TEMPERATURE_UNIT_ADDRESS,'c');
-        UART_PutString("Temperature unit set at celius\r\n");
-        /*Writing 1 on the EEPROM first cell */
-        Flag_cell = 1;
-        EEPROM_writeByte(FLAG_ADDRESS,Flag_cell);
-    }
-    CyGlobalIntEnable; /* Enable global interrupts. */
- 
-    UART_Start();
-    isr_UART_StartEx(Custom_isr_UART);
-        
-    change_settings_flag=0;
-    option_table= DONT_SHOW_TABLE;
-    initialized=0;
-    feature_selected = 0;
+    if (Flag_Cell == 0) EEPROM_Initialization();
+    
     /* array used to change the period of the timer when the user changes the sampling frequency] */
     uint16 timer_periods[4] = { 1000, 100, 40, 20 }; 
     
-    /* After tables displaying the possible setting have shown and the user 
-    * / has inseerted a valid input, the accelerometer setting or temperature data 
-    * / format are changed (da cambiare fa schifo)
-    */
-    if (option_table != DONT_SHOW_TABLE && feature_selected !=0) 
-    {  
-        /* data need to be deleted: the timer is stopped to not generate new data */
-        Timer_Stop();
-
-    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
-    Timer_Start();
-    UART_Start();
-    ADC_DelSig_Start();
     
-    isr_TIMER_StartEx(Custom_isr_TIMER);
-    ADC_DelSig_StartConvert();
+   
     FlagReady = 0;
     start = 0;
     stop = 0;
-    change_settings_flag = 1;
+    change_settings_flag = 0;
     option_table = DONT_SHOW_TABLE;
     initialized = 0;
     feature_selected = 0;
     KeysMenu = 0;
     display_error = 0;
     
-    *//*
-    I2C_Master_Start();
-    
 
 
     uint8_t EEPROM_Data[EEPROM_PACKET_BYTES * (WATERMARK_LEVEL + 1)];
-    isr_TIMER_StartEx(Custom_isr_TIMER);
-    isr_FIFO_StartEx(Custom_isr_FIFO);
     
     uint8_t i;
     
@@ -156,7 +114,6 @@ int main(void)
                 }
             }  
             
-            //function to send data to EEPROM to be put here
             FIFODataReadyFlag = 0;
             TempDataReadyFlag = 0;
     
@@ -167,6 +124,25 @@ int main(void)
             ShowMenuFlag = 0;
             KeysMenu = 1;
         }
+        
+        if(KeysMenu == 1){
+            switch (option_table){
+                case F_S_R:
+                    Show_table(0);
+                    KeysMenu = 0;
+                break;
+                case SAMP_FREQ:
+                    Show_table(1);
+                    KeysMenu = 0;
+                break;
+                case TEMP:
+                    Show_table(2);
+                    KeysMenu = 0;
+                break;
+            }
+        }
+        
+    
         /* Value of option table defines which settings have to be modified:
         * option table= FSR -> change the full scale range of the accelerometer
         * option table = SAMP_FREQ -> change the sampling frequency of the acceleromter
@@ -176,6 +152,9 @@ int main(void)
         if (option_table!= DONT_SHOW_TABLE && feature_selected) {
             switch (option_table) 
             {
+                /* data need to be deleted: the timer is stopped to not generate new data */
+                Timer_Stop();
+                
                 case F_S_R:
                     /* change full scale range */
                     Change_Accelerometer_FSR();
@@ -198,24 +177,7 @@ int main(void)
             KeysMenu=0;
             ShowMenuFlag=1;
         }
-            
-        
-        if(KeysMenu == 1){
-            switch (option_table){
-                case F_S_R:
-                    Show_table(0);
-                    KeysMenu = 0;
-                break;
-                case SAMP_FREQ:
-                    Show_table(1);
-                    KeysMenu = 0;
-                break;
-                case TEMP:
-                    Show_table(2);
-                    KeysMenu = 0;
-                break;
-            }
-        }
+         
         if(start){
             /* save the value  in the EEPROM */    
             EEPROM_writeByte(BEGIN_STOP_ADDRESS, 1);
@@ -232,12 +194,13 @@ int main(void)
             display_error = 0;
         }
     }
+}
         
     
 void Display_error(){
     UART_PutString("\nSelection invalid. Please choose one of the available characters\r\n");
 }
-}
+
 
     
 
