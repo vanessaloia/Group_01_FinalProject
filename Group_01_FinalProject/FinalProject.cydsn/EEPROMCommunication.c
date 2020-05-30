f6c705f5248c574e3ba5af5a3eed35a188fcdd9e
Beatrice Bonomi <beatrice.bonomi@mail.polimi.it>
2020-05-30 11:36:13 +0200

    EEPROM wait for write complete added in functions to store modified settings in EEPROM

--- Group_01_FinalProject/FinalProject.cydsn/EEPROMCommunication.c
+++ Group_01_FinalProject/FinalProject.cydsn/EEPROMCommunication.c
@@ -16,8 +16,7 @@
 #include "project.h"
 
 
-uint16_t Pointer;    
-uint8_t Flag_Cell;
+
 
 void EEPROM_Data_Write(void) {
     
@@ -38,31 +37,41 @@ void EEPROM_Initialization(void) {
     /*Setting registers at default value*/ 
     Pointer = FIRST_FREE_CELL;
     EEPROM_writeByte(POINTER_ADDRESS_H,(Pointer&0xFF00)>>8);
+    EEPROM_waitForWriteComplete();
     EEPROM_writeByte(POINTER_ADDRESS_L,(Pointer&0xff));
+    EEPROM_waitForWriteComplete();    
     EEPROM_writeByte(BEGIN_STOP_ADDRESS,0);
+    EEPROM_waitForWriteComplete();
     EEPROM_writeByte(FULL_SCALE_RANGE_ADDRESS,1);
+    EEPROM_waitForWriteComplete();
     EEPROM_writeByte(SAMPLING_FREQUENCY_ADDRESS,1);
+    EEPROM_waitForWriteComplete();    
     EEPROM_writeByte(TEMPERATURE_UNIT_ADDRESS,'c');
+    EEPROM_waitForWriteComplete();
     /*Writing 1 on the EEPROM first cell */
     Flag_Cell = 1;
     EEPROM_writeByte(FLAG_ADDRESS,Flag_Cell);
+    EEPROM_waitForWriteComplete();
     UART_PutString("Device started up for the first time. You can change default settings through the menu \r\n");
+
 }
 
 void EEPROM_Store_FSR(void) {
     
     EEPROM_writeByte(FULL_SCALE_RANGE_ADDRESS, feature_selected);
+    EEPROM_waitForWriteComplete();
     
 }
 
 void EEPROM_Store_Freq(void) {
     
     EEPROM_writeByte(SAMPLING_FREQUENCY_ADDRESS, feature_selected);
-    
+    EEPROM_waitForWriteComplete();
 }
 void EEPROM_Store_Temp(void) {
     
     EEPROM_writeByte(TEMPERATURE_UNIT_ADDRESS, feature_selected);
+    EEPROM_waitForWriteComplete();
     
 }
 
